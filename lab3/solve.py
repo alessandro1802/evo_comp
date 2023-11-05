1bed506405b92eeee361fc5da1e0c630fda31f12
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-11-05 22:07:08 +0100

    updated lab 3 steepest alg

--- lab3/solve.py
+++ lab3/solve.py
@@ -71,16 +71,18 @@ class Solver():
     def greedy_ls(self, startNode, init_sol_f, intra_route_type):
         # Generate initial solution
         current_sol = init_sol_f(startNode)
-
-        # 0 - intra, 1 - inter
-        neighbourhood_idx = 0
+        
         better_found = True
         while better_found:
+            # Randomly choose neighbourhood
+            if random.random() > 0.5:
+                neighbourhood_idx = 1
+            else:
+                neighbourhood_idx = 1
             # Randomly wrap around current route
             start_idx = random.choice(range(len(current_sol)))
             current_sol = current_sol[start_idx:] + current_sol[:start_idx]
         
-            # best_delta = 1
             best_route = None
             better_found = False
 
@@ -98,7 +100,6 @@ class Solver():
                                 next_j = 0
                             delta = self.getDeltaIntraNodes(current_sol[node_i_idx - 1], current_sol[node_i_idx], current_sol[node_j_idx], current_sol[next_j])
                             if delta < 0:
-                                # best_delta = delta
                                 best_route = current_sol[:node_i_idx] + current_sol[node_i_idx: node_j_idx + 1][::-1]
                                 if next_j != 0 :
                                     best_route += current_sol[next_j:]
@@ -123,7 +124,6 @@ class Solver():
                             # Using nodes themselves
                             delta = self.getDeltaIntraEdges(edge1[0], edge2[0])
                             if delta < 0:
-                                # best_delta = delta
                                 # First part, Reversed middle part, Last part, using node indicies
                                 e_idx1, e_idx2 = edge1[1], edge2[1]
                                 best_route = deepcopy(current_sol)
@@ -141,8 +141,6 @@ class Solver():
                     for node_j in not_selected:
                         delta = self.getDeltaInter(current_sol[i - 1], node_i, current_sol[i + 1], node_j)
                         if delta < 0:
-                            # best_delta = delta
-                            # best_route = current_sol.copy()
                             best_route = deepcopy(current_sol)
                             best_route[i] = node_j
                             break
@@ -150,109 +148,92 @@ class Solver():
                         current_sol = best_route
                         better_found = True
                         break
-            # Change the Neighbourhood
-            neighbourhood_idx += 1
-            if neighbourhood_idx == 2:
-                neighbourhood_idx = 0
         return current_sol
 
-    # TODO
     def steepest_ls(self, startNode, init_sol_f, intra_route_type):
         # Generate initial solution
         current_sol = init_sol_f(startNode)
 
         better_found = True
-        min_delta = np.inf
-
         while better_found:
+            # Randomly choose neighbourhood
+            if random.random() > 0.5:
+                neighbourhood_idx = 1
+            else:
+                neighbourhood_idx = 1
             # Randomly wrap around current route
-            #start_idx = random.choice(range(len(current_sol)))
-            #current_sol = current_sol[start_idx:] + current_sol[:start_idx]
-        
+            start_idx = random.choice(range(len(current_sol)))
+            current_sol = current_sol[start_idx:] + current_sol[:start_idx]
+
+            best_delta = 0
             best_route = None
             better_found = False
 
-            if intra_route_type == "nodes":
-                # Intra nodes
-                min_delta_intra = np.inf
-                for node_i_idx in range(len(current_sol[:-1])):
-                    if node_i_idx == 0:
+            if neighbourhood_idx == 0:
+                if intra_route_type == "nodes":
+                    # Intra nodes
+                    for node_i_idx in range(len(current_sol[:-1])):
+                        if node_i_idx == 0:
                             exclude = 1
-                    else:
-                        exclude = 0
+                        else:
+                            exclude = 0
                         
-                    for node_j_idx in range(node_i_idx + 1, len(current_sol) - exclude):
-                        if (next_j:= node_j_idx + 1) == len(current_sol):
-                            next_j = 0
-                        delta = self.getDeltaIntraNodes(current_sol[node_i_idx - 1], current_sol[node_i_idx], current_sol[node_j_idx], current_sol[next_j])
-                        if delta < min_delta_intra:
-                            best_route = current_sol[:node_i_idx] + current_sol[node_i_idx: node_j_idx + 1][::-1]
-                            min_delta_intra = delta
-                            if next_j != 0 :
-                                best_route += current_sol[next_j:]
-                            best_route_intra = best_route
-                            #print("Intra: ", min_delta_intra, best_route_intra)
-                            better_found = True
-                # Inter
-                min_delta_inter = np.inf
-                not_selected = list(set(self.cities) - set(current_sol))
-                for i, node_i in enumerate(current_sol[:-1]):
-                    for node_j in not_selected:
-                        delta = self.getDeltaInter(current_sol[i - 1], node_i, current_sol[i + 1], node_j)
-                        if delta < min_delta_inter:
-                            best_route = deepcopy(current_sol)
-                            best_route[i] = node_j
-                            min_delta_inter = delta
-                            best_route_inter = best_route
-                            #print("Inter: ", min_delta_inter, best_route_inter)
-                            better_found = True
-                if min_delta_inter < min_delta_intra:
-                    #print("inter is min")
-                    current_sol = best_route_inter
-                else:
-                    current_sol = best_route_intra
-                    #print("intra is min")
-            else:
-                # Intra edges
-                # Generate edges with node indicies [[node1, node2], [node1_idx, node2_idx]]
-                edges = [[[node_i, current_sol[i + 1]], [i, i + 1]] for i, node_i in enumerate(current_sol[:-1])]
-                edges.append([[current_sol[-1], current_sol[0]], [len(current_sol), 0]])
-                    
-                for edge1_idx, edge1 in enumerate(edges[:-2]):
-                    if edge1_idx == 0:
-                        exclude = -1
-                    else:
-                        exclude = len(edges)
-                        
-                    for edge2 in edges[edge1_idx + 2: exclude]:
-                        # Using nodes themselves
-                        delta = self.getDeltaIntraEdges(edge1[0], edge2[0])
-                        if delta < min_delta:
-                            # best_delta = delta
-                            # First part, Reversed middle part, Last part, using node indicies
-                            e_idx1, e_idx2 = edge1[1], edge2[1]
-                            best_route = deepcopy(current_sol)
-                            best_route = best_route[:e_idx1[1]] + best_route[e_idx1[1]: e_idx2[1]][::-1] + best_route[e_idx2[1]:]
-                            min_delta = delta
-                    if best_route:                    
-                        current_sol = best_route
-                        better_found = True
-                
-                # Inter
-                not_selected = list(set(self.cities) - set(current_sol))
-                for i, node_i in enumerate(current_sol[:-1]):
-                    for node_j in not_selected:
-                        delta = self.getDeltaInter(current_sol[i - 1], node_i, current_sol[i + 1], node_j)
-                        if delta < min_delta:
-                            # best_delta = delta
-                            # best_route = current_sol.copy()
-                            best_route = deepcopy(current_sol)
-                            best_route[i] = node_j
-                            min_delta = delta 
+                        for node_j_idx in range(node_i_idx + 1, len(current_sol) - exclude):
+                            if (next_j:= node_j_idx + 1) == len(current_sol):
+                                next_j = 0
+                            delta = self.getDeltaIntraNodes(current_sol[node_i_idx - 1], current_sol[node_i_idx], current_sol[node_j_idx], current_sol[next_j])
+                            if delta < best_delta:
+                                best_delta = delta
+                                best_route = current_sol[:node_i_idx] + current_sol[node_i_idx: node_j_idx + 1][::-1]
+                                if next_j != 0 :
+                                    best_route += current_sol[next_j:]
+                                # break
                     if best_route:
                         current_sol = best_route
                         better_found = True
-    
+                        # break
+                else:
+                    # Intra edges
+                    # Generate edges with node indicies [[node1, node2], [node1_idx, node2_idx]]
+                    edges = [[[node_i, current_sol[i + 1]], [i, i + 1]] for i, node_i in enumerate(current_sol[:-1])]
+                    edges.append([[current_sol[-1], current_sol[0]], [len(current_sol), 0]])
+                    
+                    for edge1_idx, edge1 in enumerate(edges[:-2]):
+                        if edge1_idx == 0:
+                            exclude = -1
+                        else:
+                            exclude = len(edges)
+                        
+                        for edge2 in edges[edge1_idx + 2: exclude]:
+                            # Using nodes themselves
+                            delta = self.getDeltaIntraEdges(edge1[0], edge2[0])
+                            if delta < best_delta:
+                                best_delta = delta
+                                # First part, Reversed middle part, Last part, using node indicies
+                                e_idx1, e_idx2 = edge1[1], edge2[1]
+                                best_route = deepcopy(current_sol)
+                                best_route = best_route[:e_idx1[1]] + best_route[e_idx1[1]: e_idx2[1]][::-1] + best_route[e_idx2[1]:]
+                                # break
+                    if best_route:                    
+                        current_sol = best_route
+                        better_found = True
+                        # break
+            else:
+                # Inter
+                # Get a list of not seleted nodes
+                not_selected = list(set(self.cities) - set(current_sol))
+                for i, node_i in enumerate(current_sol[:-1]):
+                    for node_j in not_selected:
+                        delta = self.getDeltaInter(current_sol[i - 1], node_i, current_sol[i + 1], node_j)
+                        if delta < best_delta:
+                            best_delta = delta
+                            best_route = deepcopy(current_sol)
+                            best_route[i] = node_j
+                            # break
+                if best_route:
+                    current_sol = best_route
+                    better_found = True
+                    # break
         return current_sol
 
     
@@ -271,33 +252,54 @@ class Solver():
 
     # TODO CHANGE
     def solve(self):
-        # 1. greedy, two-nodes exchange, random ss
+        # # 1. greedy, two-nodes exchange, random ss
+        # algorithm = "greedy, two-nodes, random"
+        # solutions = []
+        # evaluations = []
+        # # Get solutions and evaluations
+        # print(algorithm)
+        # for startNode in tqdm(self.cities):
+        #     solutions.append(self.greedy_ls(startNode, self.random_ss, "nodes"))
+        #     evaluations.append(self.getTotalDistance(solutions[-1]))
+        #     break
+        # # Get and print stats
+        # min_result, avg_result, max_result, best_sol_idx = self.calculateStats(evaluations)
+        # print(f"MIN {min_result} AVG {avg_result} MAX {max_result}")
+        # # Save best solution
+        # best_sol = solutions[best_sol_idx]
+        # outputPath = os.path.join(self.outputPath, algorithm + ".csv")
+        # self.writeRouteToCSV(best_sol, outputPath)
+        
+        # # 2. greedy, two-nodes exchange, best greedy heuristic ss
+        
+        # # 3. greedy, two-edges exchange, random ss
+        # algorithm = "greedy, two-edges, random"
+        # solutions = []
+        # evaluations = []
+        # # Get solutions and evaluations
+        # print(algorithm)
+        # for startNode in tqdm(self.cities):
+        #     solutions.append(self.greedy_ls(startNode, self.random_ss, "edges"))
+        #     evaluations.append(self.getTotalDistance(solutions[-1]))
+        #     break
+        # # Get and print stats
+        # min_result, avg_result, max_result, best_sol_idx = self.calculateStats(evaluations)
+        # print(f"MIN {min_result} AVG {avg_result} MAX {max_result}")
+        # # Save best solution
+        # best_sol = solutions[best_sol_idx]
+        # outputPath = os.path.join(self.outputPath, algorithm + ".csv")
+        # self.writeRouteToCSV(best_sol, outputPath)
+
+        # # 4. greedy, two-edges exchange, best greedy heuristic ss
+
+        # 5. steepest, two-nodes exchange, random ss
         algorithm = "steepest, two-nodes, random"
         solutions = []
         evaluations = []
         # Get solutions and evaluations
         print(algorithm)
         for startNode in tqdm(self.cities):
-            solutions.append(self.greedy_ls(startNode, self.random_ss, "nodes"))
-            evaluations.append(self.getTotalDistance(solutions[-1]))
-            break
-        # Get and print stats
-        min_result, avg_result, max_result, best_sol_idx = self.calculateStats(evaluations)
-        print(f"MIN {min_result} AVG {avg_result} MAX {max_result}")
-        # Save best solution
-        best_sol = solutions[best_sol_idx]
-        outputPath = os.path.join(self.outputPath, algorithm + ".csv")
-        self.writeRouteToCSV(best_sol, outputPath)
-        
-        # 2. greedy, two-nodes exchange, best greedy heuristic ss
-        # 3. greedy, two-edges exchange, random ss
-        algorithm = "steepest, two-edges, random"
-        solutions = []
-        evaluations = []
-        # Get solutions and evaluations
-        print(algorithm)
-        for startNode in tqdm(self.cities):
-            solutions.append(self.greedy_ls(startNode, self.random_ss, "edges"))
+            solutions.append(self.steepest_ls(startNode, self.random_ss, "nodes"))
             evaluations.append(self.getTotalDistance(solutions[-1]))
             break
         # Get and print stats
@@ -308,11 +310,28 @@ class Solver():
         outputPath = os.path.join(self.outputPath, algorithm + ".csv")
         self.writeRouteToCSV(best_sol, outputPath)
 
-        # 4. greedy, two-edges exchange, best greedy heuristic ss
-        # 5. steepest, two-nodes exchange, random ss
         # 6. steepest, two-nodes exchange, best greedy heuristic ss
+        
         # 7. steepest, two-edges exchange, random ss
+        algorithm = "steepest, two-edges, random"
+        solutions = []
+        evaluations = []
+        # Get solutions and evaluations
+        print(algorithm)
+        for startNode in tqdm(self.cities):
+            solutions.append(self.steepest_ls(startNode, self.random_ss, "edges"))
+            evaluations.append(self.getTotalDistance(solutions[-1]))
+            break
+        # Get and print stats
+        min_result, avg_result, max_result, best_sol_idx = self.calculateStats(evaluations)
+        print(f"MIN {min_result} AVG {avg_result} MAX {max_result}")
+        # Save best solution
+        best_sol = solutions[best_sol_idx]
+        outputPath = os.path.join(self.outputPath, algorithm + ".csv")
+        self.writeRouteToCSV(best_sol, outputPath)
+
         # 8. steepest, two-edges exchange, best greedy heuristic ss
+
 
 
 if __name__ == "__main__":
