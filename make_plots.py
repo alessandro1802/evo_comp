0065d062845f3e12e918598860f19c68c8729fb3
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-11-09 15:31:49 +0100

    updated plotting script

--- make_plots.py
+++ make_plots.py
@@ -1,5 +1,5 @@
-from glob import glob
 import os
+from glob import glob
 
 import numpy as np
 import pandas as pd
@@ -8,29 +8,35 @@ import matplotlib.pyplot as plt
 
 
 instancesPath = "./instances/"
-solutionsPath = "./lab2/solutions/"
-outputPath = "./lab2/plots/"
+labPath = "./lab2/"
 
-colors = ['red', 'green', 'blue']
 
+solutionsPath = os.path.join(labPath, "solutions/")
+outputPath =  os.path.join(labPath, "plots/")
+
+colors = ['red', 'green', 'blue', "cyan", "magenta", "yellow", "orange", "brown"]
 
 for instancePath in glob(os.path.join(instancesPath, "*.csv")):
     # Read raw Instance
     instance = pd.read_csv(instancePath, sep=';', header=None).T.to_numpy()
     xs, ys, costs = instance
-    
+    # Read solutions
     instanceName = instancePath.split('/')[-1].split('.')[0]
     edges = []
     solutionNames = []
-    for edgesPath in glob(os.path.join(solutionsPath, instanceName, "*.csv")):
+    edgesPaths = glob(os.path.join(solutionsPath, instanceName, "*.csv"))
+    for edgesPath in sorted(edgesPaths):
         # Read generated edges
         tmp1 = pd.read_csv(edgesPath, sep=';', header=None).to_numpy().squeeze()
-        tmp2 = np.roll(tmp1, 1)
+        tmp2 = np.roll(tmp1, -1)
         edges.append(np.stack([tmp1, tmp2]).T)
 
         solutionNames.append(edgesPath.split('/')[-1].split('.')[0])
-    # Make plots
-    fig, axs = plt.subplots(1, len(solutionNames), figsize=(16, 5), dpi = 200)
+    # Make plot
+    width = 16
+    width = width + width * int(len(edgesPaths) // 8)
+    figsize = (width, 5)
+    fig, axs = plt.subplots(1, len(solutionNames), figsize=figsize, dpi = 150)
     fig.suptitle(instanceName, fontsize=20)
     
     for i, ax in enumerate(axs.flat):
@@ -38,23 +44,12 @@ for instancePath in glob(os.path.join(instancesPath, "*.csv")):
         ax.scatter(xs, ys, s=costs/10)
         # Plot edges
         for idx1, idx2 in edges[i]:
-            ax.plot([xs[idx1], xs[idx2]], [ys[idx1], ys[idx2]], 
+            ax.plot([xs[idx1], xs[idx2]], 
+                    [ys[idx1], ys[idx2]], 
                      color=colors[i])
         
         ax.set_title(solutionNames[i])
         ax.set(xlabel='x', ylabel='y')
         ax.label_outer()
-    # Save
+    # Save plot
     fig.savefig(os.path.join(outputPath, instanceName + ".jpg"))
-
-    # # Singe plot
-    # fig = plt.figure(figsize=(16, 5), dpi = 200) 
-    # fig.suptitle(instanceName, fontsize=20)
-    # i = 0
-    # # Plot coords
-    # plt.scatter(xs, ys, s=costs/10)
-    # # Plot edges
-    # for idx1, idx2 in edges[i]:
-    #     plt.plot([xs[idx1], xs[idx2]], [ys[idx1], ys[idx2]], 
-    #              color=colors[i])
-    # fig.savefig('./' + instanceName + ".jpg")
