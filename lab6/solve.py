adeec2dfb4784d5a8dfe21d78165b9282897179d
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-12-03 16:07:16 +0100

    added Lab 6 ILS

--- lab6/solve.py
+++ lab6/solve.py
@@ -1,6 +1,5 @@
 import csv
 import os
-import sys
 import random
 from glob import glob
 from copy import deepcopy
@@ -109,6 +108,7 @@ class Solver_LS():
                 better_found = True
         return current_sol
 
+    
     def msls(self, nIterations = 200):
         solutions, evaluations = [], []
         for _ in range(nIterations):
@@ -120,31 +120,49 @@ class Solver_LS():
         best_sol_idx = np.argmin(evaluations)
         return solutions[best_sol_idx], evaluations[best_sol_idx]
 
-    #TODO
-    # def perturb(solution):
-    #     pass
+    
+    def new_vertex(self, solution):
+        new_sol = solution.copy()
+        not_selected = list(set(self.cities) - set(new_sol))
+        edge1 = random.randint(0, len(new_sol)-1)
+        new_vertex = random.choice(not_selected)
+        new_sol[edge1] = new_vertex
+        return new_sol
+
+    def perturb(self, solution):        
+        new_sol1 = self.new_vertex(solution)
+        new_sol2 = self.new_vertex(new_sol1)
+        new_sol3 = self.new_vertex(new_sol2)
+        new_sol4 = self.new_vertex(new_sol3)
+        return new_sol4
         
-    # def ils(self, max_time_seconds): #TODO
-    #     startNode = np.random.randint(len(self.cities))
-    #     current_sol = self.random_ss(startNode)
-        
-    #     start_time = time()
-    #     while time() - start_time < max_time_seconds:
-    #         # LS
-    #         improved_solution = self.steepest_ls(current_sol)
-    #         current_obj = self.getTotalDistance(improved_solution)
-    #         # LS on perturbed solution
-    #         perturbed_solution = self.perturb(improved_solution)
-    #         intensified_solution =  self.steepest_ls(perturbed_solution)
-    #         new_obj = self.getTotalDistance(intensified_solution)
-    #         # Acceptance criterion
-    #         if new_obj < current_obj:
-    #             current_sol = intensified_solution
-    #         # Assign best solution
-    #         if new_obj < best_obj:
-    #             best_obj = new_obj
-    #             best_sol = intensified_solution
-    #     return best_sol, best_obj
+    def ils(self, max_time_seconds):
+        startNode = np.random.randint(len(self.cities))
+        current_sol = self.random_ss(startNode)
+
+        best_obj = np.inf
+        best_sol = None
+
+        lsRuns = 0
+        start_time = time()
+        while time() - start_time < max_time_seconds:
+            # LS
+            improved_solution = self.steepest_ls(current_sol)
+            current_obj = self.getTotalDistance(improved_solution)
+            # LS on perturbed solution
+            perturbed_solution = self.perturb(improved_solution)
+            intensified_solution =  self.steepest_ls(perturbed_solution)
+            new_obj = self.getTotalDistance(intensified_solution)
+
+            lsRuns += 2
+            # Acceptance criterion
+            if new_obj < current_obj:
+                current_sol = intensified_solution
+            # Assign best solution
+            if new_obj < best_obj:
+                best_obj = new_obj
+                best_sol = intensified_solution
+        return best_sol, best_obj, lsRuns
 
     
     def calculateStats(self, evaluations):
@@ -161,32 +179,14 @@ class Solver_LS():
             
 
     def solve(self):        
-        algorithm = "msls"
-        solutions, evaluations = [], []
-        # Get solutions and evaluations
-        print(algorithm)    
-        for _ in tqdm(range(20)):
-            sol, eval = self.msls()
-            solutions.append(sol)
-            evaluations.append(eval)        
-        # Get and print stats
-        stats, best_sol_idx = self.calculateStats(evaluations)
-        print(stats)
-        # Save best solution
-        best_sol = solutions[best_sol_idx]
-        outputPath = os.path.join(self.outputPath, algorithm + ".csv")
-        self.writeRouteToCSV(best_sol, outputPath)
-
-        # # TODO
-        # algorithm = "ils"
+        # algorithm = "msls"
         # solutions, evaluations = [], []
         # # Get solutions and evaluations
-        # print(algorithm)
-        # max_time_seconds = #TODO
+        # print(algorithm)    
         # for _ in tqdm(range(20)):
-        #     sol, eval = self.ils(max_time_seconds)
+        #     sol, eval = self.msls()
         #     solutions.append(sol)
-        #     evaluations.append(eval)
+        #     evaluations.append(eval)        
         # # Get and print stats
         # stats, best_sol_idx = self.calculateStats(evaluations)
         # print(stats)
@@ -194,6 +194,25 @@ class Solver_LS():
         # best_sol = solutions[best_sol_idx]
         # outputPath = os.path.join(self.outputPath, algorithm + ".csv")
         # self.writeRouteToCSV(best_sol, outputPath)
+
+        algorithm = "ils"
+        solutions, evaluations, runs = [], [], []
+        # Get solutions and evaluations
+        print(algorithm)
+        max_time_seconds = 320
+        for _ in tqdm(range(20)):
+            sol, eval, lsRuns = self.ils(max_time_seconds)
+            solutions.append(sol)
+            evaluations.append(eval)
+            runs.append(lsRuns)
+        # Get and print stats
+        stats, best_sol_idx = self.calculateStats(evaluations)
+        print(stats)
+        print("LS runs:", np.mean(runs))
+        # Save best solution
+        best_sol = solutions[best_sol_idx]
+        outputPath = os.path.join(self.outputPath, algorithm + ".csv")
+        self.writeRouteToCSV(best_sol, outputPath)
         
 
 if __name__ == "__main__":
