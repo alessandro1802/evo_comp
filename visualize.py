31276999261a360020a243cb21fc1a0e672cfc11
Alessandro <oleksandr.yasinskyi@icloud.com>
2023-12-04 10:19:38 +0100

    updated plot making script

--- visualize.py
+++ visualize.py
@@ -7,10 +7,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+labPath = "./lab6/"
+width = 16
+
+
 instancesPath = "./instances/"
-labPath = "./lab2/"
-
-
 solutionsPath = os.path.join(labPath, "solutions/")
 outputPath =  os.path.join(labPath, "plots/")
 
@@ -22,34 +23,30 @@ for instancePath in glob(os.path.join(instancesPath, "*.csv")):
     xs, ys, costs = instance
     # Read solutions
     instanceName = instancePath.split('/')[-1].split('.')[0]
-    edges = []
-    solutionNames = []
-    edgesPaths = glob(os.path.join(solutionsPath, instanceName, "*.csv"))
-    for edgesPath in sorted(edgesPaths):
+    edges, solutionNames = [], []
+    for solutionPath in sorted(glob(os.path.join(solutionsPath, instanceName, "*.csv"))):
         # Read generated edges
-        tmp1 = pd.read_csv(edgesPath, sep=';', header=None).to_numpy().squeeze()
+        tmp1 = pd.read_csv(solutionPath, sep=';', header=None).to_numpy().squeeze()
         tmp2 = np.roll(tmp1, -1)
         edges.append(np.stack([tmp1, tmp2]).T)
 
-        solutionNames.append(edgesPath.split('/')[-1].split('.')[0])
+        solutionNames.append(solutionPath.split('/')[-1].split('.')[0])
     # Make plot
-    width = 16
-    width = width + width * int(len(edgesPaths) // 8)
     figsize = (width, 5)
     fig, axs = plt.subplots(1, len(solutionNames), figsize=figsize, dpi = 150)
-    fig.suptitle(instanceName, fontsize=20)
     
-    for i, ax in enumerate(axs.flat):
+    fig.suptitle(instanceName, fontsize=20)
+    for i, ax in enumerate(fig.axes):
         # Plot coords
         ax.scatter(xs, ys, s=costs/10)
         # Plot edges
         for idx1, idx2 in edges[i]:
             ax.plot([xs[idx1], xs[idx2]], 
                     [ys[idx1], ys[idx2]], 
-                     color=colors[i])
+                    color=colors[i])
         
         ax.set_title(solutionNames[i])
         ax.set(xlabel='x', ylabel='y')
-        ax.label_outer()
+        ax.label_outer()    
     # Save plot
     fig.savefig(os.path.join(outputPath, instanceName + ".jpg"))
